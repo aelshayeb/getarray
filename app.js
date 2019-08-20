98ad16571a6de88c482f6b97faab6e9439c2256d
aelshayeb <ali.livenet.el@gmail.com>
2019-08-20 07:39:24 -0400

    Add ebextension

--- app.js
+++ app.js
@@ -87,6 +87,6 @@ app.post('/signup', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, console.log(`Server started on ${PORT}`));
